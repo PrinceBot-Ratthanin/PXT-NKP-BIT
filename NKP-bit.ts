33fc1310a1f90c54e85bd6eac1f69050830a8b65
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-03-08 19:52:01 +0700

    Update NKP-bit.ts

--- NKP-bit.ts
+++ NKP-bit.ts
@@ -41,51 +41,105 @@ enum Servo {
 	//% block="P16"
 	Servo16
     }
-enum APort {
-	//% block="P0"
-	P0,
-	//% block="P1"
-	P1,
-	//% block="P2"
-	P2,
-	//% block="P3"
-	P3,
-	//% block="P4"
-	P4,
-	//% block="P10"
-	P10
-    }
-enum digitalPort {
-        //% block="P0"
-	P0,
-	//% block="P1"
-	P1,
-	//% block="P2"
-	P2,
-	//% block="P3"
-	P3,
-	//% block="P4"
-	P4,
-	//% block="P10"
-	P10,
-	//% block="P13"
-	P13,
-	//% block="P14"
-	P14,
-	//% block="P15"
-	P15,
-	//% block="P16"
-	P16
-    }
-
 
    
-
 /**
  * Custom blocks
  */
 //% weight=10 color=#ff9900 weight=10 icon="\uf11b"
 namespace NKP_BIT {
+    export enum analogPort {
+        P0,
+        P1,
+        P2,
+        P3,
+        P4,
+        P10
+    }
+
+    export enum digitalPort {
+        P0,
+        P1,
+        P2,
+        P3,
+        P4,
+        P10,
+        P6,
+        P7,
+        P8,
+        P9,
+        P12,
+        P5,
+        P11
+    }
+
+    /**
+     * read analog sensor value from P0 -P4 and P10
+     * @param selectpin         select analog pin to read
+     * @return number           returns analog value from 0 to 1023
+     */
+    //% blockId=MySensor_analogRead
+    //% block="analog read |%selectpin|"
+    //% weight=80
+    export function analogRead(selectpin: analogPort): number {
+        switch (selectpin) {
+            case analogPort.P0:
+                return pins.analogReadPin(AnalogPin.P0);
+            case analogPort.P1:
+                return pins.analogReadPin(AnalogPin.P1);
+            case analogPort.P2:
+                return pins.analogReadPin(AnalogPin.P2);
+            case analogPort.P3:
+                return pins.analogReadPin(AnalogPin.P3);
+            case analogPort.P4:
+                return pins.analogReadPin(AnalogPin.P4);
+            case analogPort.P10:
+                return pins.analogReadPin(AnalogPin.P10);
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * read Digital sensor value from P0-P12
+     * @param selectpins         select digital pin to read
+     * @return number           returns digital value  0 or 1
+     */
+    //% blockId=MySensor_digitalRead
+    //% block="digital read |%selectpins|"
+    //% weight=79
+    export function digitalRead(selectpins:digitalPort): number {
+        switch (selectpins) {
+            case digitalPort.P0:
+                return pins.digitalReadPin(DigitalPin.P0);
+            case digitalPort.P1:
+                return pins.digitalReadPin(DigitalPin.P1);
+            case digitalPort.P2:
+                return pins.digitalReadPin(DigitalPin.P2);
+            case digitalPort.P3:
+                return pins.digitalReadPin(DigitalPin.P3);
+            case digitalPort.P4:
+                return pins.digitalReadPin(DigitalPin.P4);
+            case digitalPort.P5:
+                return pins.digitalReadPin(DigitalPin.P5);
+            case digitalPort.P6:
+                return pins.digitalReadPin(DigitalPin.P6);
+            case digitalPort.P7:
+                return pins.digitalReadPin(DigitalPin.P7);
+            case digitalPort.P8:
+                return pins.digitalReadPin(DigitalPin.P8);
+            case digitalPort.P9:
+                return pins.digitalReadPin(DigitalPin.P9);
+            case digitalPort.P10:
+                return pins.digitalReadPin(DigitalPin.P10);
+            case digitalPort.P11:
+                return pins.digitalReadPin(DigitalPin.P11);
+            case digitalPort.P12:
+                return pins.digitalReadPin(DigitalPin.P12);
+            default:
+                return 0;
+        }
+    }
 
     /**MotorON          Control motor channel direction and speed.   
     * @param Speed  	  Percent of motor speed, eg: 50
@@ -149,74 +203,6 @@ namespace NKP_BIT {
         
     }
 	
-    //% blockId=NKP_AnalogRead
-    //% block="AnalogRead Pin %APort"
-    //% weight=80
-    export function analogRead(selectpinA:APort): number {
-	    if(selectpinA == APort.P0){
-	    	return pins.analogReadPin(AnalogPin.P0);
-	    }
-	    
-	/*
-        switch (selectpin) {
-            case analogPort.P0:
-                return pins.analogReadPin(AnalogPin.P0);
-            case analogPort.P1:
-                return pins.analogReadPin(AnalogPin.P1);
-            case analogPort.P2:
-                return pins.analogReadPin(AnalogPin.P2);
-            case analogPort.P3:
-                return pins.analogReadPin(AnalogPin.P3);
-            case analogPort.P4:
-                return pins.analogReadPin(AnalogPin.P4);
-            case analogPort.P10:
-                return pins.analogReadPin(AnalogPin.P10);
-            default:
-                return 0;
-        }*/
-    }
-
-    /**
-     * read Digital sensor value from P0-P12
-     * @param selectpins         select digital pin to read
-     * @return number           returns digital value  0 or 1
-     */
-    //% blockId=MySensor_digitalRead
-    //% block="digital read |%selectpins|"
-    //% weight=79
-    export function digitalRead(selectpins:digitalPort): number {
-        switch (selectpins) {
-            case digitalPort.P0:
-                return pins.digitalReadPin(DigitalPin.P0);
-            case digitalPort.P1:
-                return pins.digitalReadPin(DigitalPin.P1);
-            case digitalPort.P2:
-                return pins.digitalReadPin(DigitalPin.P2);
-            case digitalPort.P3:
-                return pins.digitalReadPin(DigitalPin.P3);
-            case digitalPort.P4:
-                return pins.digitalReadPin(DigitalPin.P4);
-            case digitalPort.P5:
-                return pins.digitalReadPin(DigitalPin.P5);
-            case digitalPort.P6:
-                return pins.digitalReadPin(DigitalPin.P6);
-            case digitalPort.P7:
-                return pins.digitalReadPin(DigitalPin.P7);
-            case digitalPort.P8:
-                return pins.digitalReadPin(DigitalPin.P8);
-            case digitalPort.P9:
-                return pins.digitalReadPin(DigitalPin.P9);
-            case digitalPort.P10:
-                return pins.digitalReadPin(DigitalPin.P10);
-            case digitalPort.P11:
-                return pins.digitalReadPin(DigitalPin.P11);
-            case digitalPort.P12:
-                return pins.digitalReadPin(DigitalPin.P12);
-            default:
-                return 0;
-        }
-    }
-
 /**
  * Execute puase time
  * @param pausetime  	mSec to delay; eg: 100
